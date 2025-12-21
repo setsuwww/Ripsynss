c2e6cb7b5e2bdeaaff8b61df89018ad74d30817e
setsuwww <setsuprojectonly@gmail.com>
2025-12-21 20:33:37 +0700

    UI update

--- src/components/common/PageTitle.jsx
+++ src/components/common/PageTitle.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const TitleSection = ({ text, highlight, isHeading = false, description = ""}) => {
   return (
     <>
-      <h1 className={`font-extrabold mb-6 text-white
+      <h1 className={`font-extrabold mb-6
         ${isHeading
           ? "text-5xl sm:text-6xl lg:text-7xl"
           : "text-4xl sm:text-5xl lg:text-6xl"}
@@ -14,7 +14,7 @@ const TitleSection = ({ text, highlight, isHeading = false, description = ""}) =
       </h1>
 
       {description && (
-        <p className="h2-responsive text-slate-400 max-w-sm mx-auto" data-aos="zoom-out">
+        <p className="h2-responsive text-slate-400 dark:text-slate-600 max-w-sm mx-auto" data-aos="zoom-out">
           {description}
         </p>
       )}
--- src/constants/balls.js
+++ src/constants/balls.js
@@ -1,5 +1,5 @@
 export const balls = [
-  { className: 'top-1/4 left-1/4 w-62 h-62 bg-sky-300/10 dark:bg-yellow-300/15 blur-3xl' },
-  { className: 'bottom-1/4 right-1/4 w-86 h-86 bg-violet-500/10 dark:bg-violet-500/15 blur-3xl' },
-  { className: 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-54 h-54 bg-rose-400/10 dark:bg-rose-400/15 blur-2xl' },
+  { className: 'top-1/4 left-1/4 w-62 h-62 bg-sky-300/10 dark:bg-yellow-300/10 blur-3xl' },
+  { className: 'bottom-1/4 right-1/4 w-86 h-86 bg-violet-500/10 dark:bg-violet-500/10 blur-3xl' },
+  { className: 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-54 h-54 bg-rose-400/10 dark:bg-rose-400/10 blur-2xl' },
 ]
