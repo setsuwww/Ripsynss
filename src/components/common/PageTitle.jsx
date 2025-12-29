8f7409c402abd1bed49620e0f02ceec327a4628e
setsuwww <setsuprojectonly@gmail.com>
2025-12-29 17:32:50 +0700

    Bagusan begini, dibanding full DS

--- src/components/common/PageTitle.jsx
+++ src/components/common/PageTitle.jsx
@@ -3,14 +3,14 @@ import React from "react"
 const TitleSection = ({ text, highlight, isHeading = false, description = ""}) => {
   return (
     <>
-      <h1 className={`mb-6
+      <h1 className={`font-semibold mb-6
         ${isHeading
-          ? "flex flex-col space-y-2 text-5xl sm:text-6xl lg:text-7xl"
+          ? "text-5xl sm:text-6xl lg:text-7xl"
           : "text-4xl sm:text-5xl lg:text-6xl"}
         `} data-aos = "zoom-in"
       >
         <span className="apple-font shiny-text">{text} </span>
-        {highlight && <span className="apple-font font-extrabold gradient-text">{highlight}</span>}
+        {highlight && <span className="gradient-text">{highlight}</span>}
       </h1>
 
       {description && (
